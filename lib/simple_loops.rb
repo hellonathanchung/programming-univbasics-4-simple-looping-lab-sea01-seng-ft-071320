054883217be88ff0233a2cc5d628786eca9492d0
Nathan Chung <chung.nathanh@gmail.com>
2020-06-15 01:11:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,11 @@
 # Write your methods here
 
-def loop_message_five_times()
+def loop_message_five_times(message)
+  count = 0 
+  while count < 5 do
+    puts message
+    count += 1 
+  end
 end
 
 def loop_message_n_ntimes
