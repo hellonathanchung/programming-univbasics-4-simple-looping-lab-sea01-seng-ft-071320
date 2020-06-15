cdb948064aecaca86409d60767e7541a81c5b079
Nathan Chung <chung.nathanh@gmail.com>
2020-06-15 01:18:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,9 +25,10 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  count = 0 
+  count = 0
+  new_array = []
   while count < array.length do
-    array[count].to_s
+    new_array.push[count].to_s
     count += 1 
   end
   
